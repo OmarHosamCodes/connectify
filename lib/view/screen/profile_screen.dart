cd0b3439e99556a8d7ea1bb3c93ff8839dc1ec84
omarhosamcodes <omarhosamcodes@gmail.com>
2024-03-23 04:25:32 +0200

    hot fix

--- lib/view/screen/profile_screen.dart
+++ lib/view/screen/profile_screen.dart
@@ -9,6 +9,75 @@ class ProfileScreen extends StatelessWidget {
   //TODO: Implement ProfileScreen
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const CustomText(text: 'Profile'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.fromBorderSide(
+                    BorderSide(
+                      color: Color(0xFF0f0f0f),
+                      width: 5,
+                    ),
+                  ),
+                ),
+                child: CustomImage(
+                  source: authInstance.currentUser!.photoURL.toString(),
+                  fit: BoxFit.cover,
+                  fallbackIcon: Icons.person,
+                ),
+              ),
+              const CustomGap(),
+              CustomText(
+                text: authInstance.currentUser!.displayName!,
+                isHeadline: true,
+              ),
+              Consumer(
+                builder: (_, WidgetRef ref, __) {
+                  final userWatcher = ref.watch(getUserProvider);
+
+                  return userWatcher.when(
+                    data: (user) {
+                      return CustomText(
+                        text: user.userType!.name,
+                      );
+                    },
+                    loading: () => const CustomText(text: ''),
+                    error: (error, _) =>
+                        const CustomText(text: 'error occurred'),
+                  );
+                },
+              ),
+              ListTile(
+                title: const CustomText(text: 'Email'),
+                subtitle: CustomText(
+                  text: authInstance.currentUser!.email!,
+                ),
+              ),
+              const CustomGap(),
+              authInstance.currentUser!.phoneNumber != null
+                  ? ListTile(
+                      title: const CustomText(text: 'Phone Number'),
+                      subtitle: CustomText(
+                        text: authInstance.currentUser!.phoneNumber!,
+                      ),
+                    )
+                  : const SizedBox(),
+              const CustomGap(),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/view/screen/search_screen.dart
+++ lib/view/screen/search_screen.dart
@@ -1,116 +1,30 @@
 import 'package:connectify/library.dart';
 
-<<<<<<< HEAD
 //TODO: Redo with simpler logic
 Set<String> dummydata = {
-=======
-List<String> dummydata = [
->>>>>>> 715f9ad5af1fe335b73a0c5cb4d68c8ce7aff374
   'apple',
   'orange',
   'juice',
   'paper',
   'board',
   'python',
-<<<<<<< HEAD
 };
-=======
-];
->>>>>>> 715f9ad5af1fe335b73a0c5cb4d68c8ce7aff374
 
 final searchTextProvider = StateProvider<String?>((ref) => null);
 final existProvider = StateProvider<List<String>>((ref) => []);
 final widgetsProvider = StateProvider<Map<String, Widget>>((ref) => {});
 
 class SearchScreen extends ConsumerWidget {
-<<<<<<< HEAD
   const SearchScreen({super.key});
   static final controller = TextEditingController();
   @override
   Widget build(BuildContext context, ref) {
     return Scaffold(
       extendBody: true,
-=======
-  SearchScreen({super.key});
-  final controller = TextEditingController();
-  @override
-  Widget build(BuildContext context, ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: SearchBar(
-          controller: controller,
-          onChanged: (value) {
-            print(value);
-            ref.read(searchTextProvider.notifier).update((state) => value);
-            print('notifier value is : ${ref.read(searchTextProvider)}');
-            print('notifier value is : ${ref.read(widgetsProvider)}');
-            for (String element in dummydata) {
-              if (element.contains(ref.read(searchTextProvider)!) &&
-                  value != '') {
-                print(element + 'added element');
-                ref.read(existProvider.notifier).update((existState) {
-                  if (existState.contains(element)) {
-                    return existState;
-                  } else {
-                    existState.add(element);
-                    ref.read(widgetsProvider.notifier).update((state) => {});
-                    for (var element in existState) {
-                      ref.read(widgetsProvider.notifier).update((state) {
-                        state.addAll({
-                          element: Container(
-                            margin: const EdgeInsets.fromLTRB(
-                                0,
-                                SimpleConstants.vertiacalSpace,
-                                0,
-                                SimpleConstants.vertiacalSpace),
-                            child: morphingGlasslistTile(
-                                // height: context.height / 6,
-                                // width: context.width,
-                                roundedValue: SimpleConstants.radius,
-                                opacity: 0.4,
-                                borderColor: Colors.white.withOpacity(0.4),
-                                text: CustomText(text: element),
-                                subtitle: CustomText(text: element),
-                                leading: const CircleAvatar(
-                                  radius: SimpleConstants.radius * 1.25,
-                                ),
-                                gradientColors: [
-                                  Colors.pink.withOpacity(0.4),
-                                  Colors.white.withOpacity(0.1)
-                                ]),
-                          )
-                        });
-                        return state;
-                      });
-                    }
-                  }
-                  return existState;
-                });
-                print(ref.read(existProvider));
-              } else {
-                ref.read(existProvider.notifier).update((existState) {
-                  if (existState.contains(element)) {
-                    existState.remove(element);
-                    ref.read(widgetsProvider.notifier).update((state) {
-                      state.remove(element);
-                      print('notifier value is : ${ref.read(widgetsProvider)}');
-
-                      return state;
-                    });
-                  }
-                  return existState;
-                });
-              }
-            }
-          },
-        ),
-      ),
->>>>>>> 715f9ad5af1fe335b73a0c5cb4d68c8ce7aff374
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(
               horizontal: SimpleConstants.horizontalSpace),
-<<<<<<< HEAD
           child: Column(
             children: [
               SizedBox(
@@ -222,10 +136,6 @@ class SearchScreen extends ConsumerWidget {
                 ),
               ),
             ],
-=======
-          child: ListView(
-            children: List.from(ref.watch(widgetsProvider).values),
->>>>>>> 715f9ad5af1fe335b73a0c5cb4d68c8ce7aff374
           ),
         ),
       ),
